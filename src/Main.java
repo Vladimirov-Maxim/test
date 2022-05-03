8ccc99ce526583db6d4a94fc56484d41edb06814
Vladimirov Maxim <Vladimiroffmax@yandex.ru>
2022-05-03 13:05:51 +0300

    Bag_delete

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Bax_fix");
-        System.out.println("Bax_fix2");
-        System.out.println("Bax_fix3");
 
     }
 }
