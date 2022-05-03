702d16870b925f5c13cc1aa4fc598f835f15e7ad
Vladimirov Maxim <Vladimiroffmax@yandex.ru>
2022-05-03 13:02:19 +0300

    Bag_fix_3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Bax_fix");
+        System.out.println("Bax_fix2");
+        System.out.println("Bax_fix3");
 
     }
 }
