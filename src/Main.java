b4be5985bb62300ccb14d1d2ac624a94e0234b6a
Vladimirov Maxim <Vladimiroffmax@yandex.ru>
2022-05-05 00:29:22 +0300

    New functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getNumberWithSeparator("12345678"));
+    }
 
-        System.out.println("Bax_fix");
-        System.out.println("Bag_fix_4");
+    public static String getNumberWithSeparator(String number) {
+
+        StringBuilder result = new StringBuilder();
+        String stringForHandling = new StringBuilder(number).reverse().toString();
+
+        Pattern pattern = Pattern.compile("\\d{1,3}");
+        Matcher matcher = pattern.matcher(stringForHandling);
+
+        while (matcher.find()) {
+
+            int start = matcher.start();
+            int end = matcher.end();
+
+            if (!result.toString().equals("")) {
+                result.append(",");
+            }
+
+            result.append(number.substring(start, end));
+
+        }
+
+        return result.reverse().toString();
 
     }
+
 }
